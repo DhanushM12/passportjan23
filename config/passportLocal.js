8ecfe433618b87393c47c8e97d0d0271330cebe0
DhanushM12 <dhanushmukhi@gmail.com>
2023-08-16 22:07:16 +0530

    added passport config

---
+++ config/passportLocal.js
@@ -0,0 +1,30 @@
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+
+function initialize(passport, getUserByEmail, getUserById){
+    const authenticator = async (email, password, done) => {
+        const user = getUserByEmail(email);
+        if (!user) { // the email is not having any user
+            return done(null, false); 
+        }
+        try {
+            if(await bcrypt.compare(password, user.password)){// user access granted
+                return done(null, user);
+            } else {
+                return done(null, false); // password incorrect
+            }
+        } catch (err) {
+            return done(err);
+        } 
+    }
+    passport.use(new LocalStrategy({usernameField: 'email'}, authenticator));
+    passport.serializeUser(function(user, done) {
+        done(null, user.id);
+      });
+      
+    passport.deserializeUser(function(id, done) {
+          done(err, getUserById(id));
+      });
+}
+
+module.exports = initialize;
---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port =8080;
+
+app.listen(port, function(err){
+    if(err){
+        console.log(`Error in running the server: ${err}`);
+        return;
+    }
+    console.log(`Successfully running on port : ${port}`)
+})
